b5a2d8fa95482f9ca6fdfe2e74c201fac31c2a67
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-20 04:21:23 +0700

    Update Knew.py

--- Knew.py
+++ Knew.py
@@ -47,9 +47,9 @@ def select_logo(background_image):
     if background_image == "Bank/K-bank 4.png":
         logo_size = (130, 130)
         logo_position = (45, 540)  # ตัวอย่างตำแหน่งโลโก้สำหรับพื้นหลัง 1
-    elif background_image == "Bank/K-bank 2.png":
-        logo_size = (150, 150)
-        logo_position = (60, 1200)  # ตัวอย่างตำแหน่งโลโก้สำหรับพื้นหลัง 2
+    elif background_image == "Bank/K-bank 3.png":
+        logo_size = (130, 130)
+        logo_position = (45, 540)  # ตัวอย่างตำแหน่งโลโก้สำหรับพื้นหลัง 2
     else:
         logo_size = (120, 120)
         logo_position = (70, 1300)  # ตัวอย่างตำแหน่งโลโก้สำหรับพื้นหลัง 3
