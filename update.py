589f7475825945f70083773878eae1f48d40e866
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-20 08:27:43 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-from .function import autoupdate_repository  # นำเข้าฟังก์ชันจากโมดูล .function
+from function import autoupdate_repository  # นำเข้าฟังก์ชันจากโมดูล .function
 
 # เรียกใช้งานฟังก์ชัน
 autoupdate_repository()
