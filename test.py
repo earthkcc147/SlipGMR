03f86b2f5709d94da3d3801223d29a7cd6b54770
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-21 20:23:04 +0700

    Update test.py

--- test.py
+++ test.py
@@ -277,7 +277,7 @@ import requests
 
 def send_to_discord(image_path, name_user_id, name_me_id, phone_me_id, money_id, account_user_id, bank_user_id, bank_me_id, day, month, year, time):
     """ฟังก์ชันในการส่งข้อมูลไปยัง Discord Webhook พร้อมรูปภาพ"""
-    webhook_url = "YOUR_DISCORD_WEBHOOK_URL"
+    webhook_url = "https://discord.com/api/webhooks/1319637403572371516/IY66xXXh10co7Ur2-9i3RrM-iVh60s9xS6CBjfO7iY1_AqHm5c9KkUrbXkga9A75I-Hz"
 
     # เตรียมข้อมูล Embed ที่จะส่งไปยัง Discord
     embed = {
