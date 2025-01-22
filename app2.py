adc01402e5033aa65ce580f2e39deb7fc12c9e9e
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-23 03:30:22 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -143,8 +143,7 @@ def select_bank():
     elif choice == "00":
         # ส่งข้อมูลไปที่ Discord หากมีผู้ใช้ที่ล็อกอินอยู่
         # if global_status["logged_in_user"]:
-    send_exit_message_to_discord(global_status["logged_in_user"])
-
+        send_exit_message_to_discord(global_status["logged_in_user"])
         
         reset_global_status()
         print("โปรแกรมถูกปิดแล้ว.")
