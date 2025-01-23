18d870cf0b174d875b9c5312198b83c14fea3e0c
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-24 00:00:16 +0700

    Update banners.py

--- Banners/banners.py
+++ Banners/banners.py
@@ -68,14 +68,14 @@ def print_login():
 
 
 # สร้างข้อความ ASCII art ด้วย pyfiglet
-EMAIL = pyfiglet.figlet_format("SPAM MAIL", font="calvin_s", width=80)
+HOME = pyfiglet.figlet_format("Welcome\nยินดีต้อนรับ", font="calvin_s", width=80)
 
 # ฟังก์ชันแสดงข้อความพร้อมดีเลย์
-def email():
+def HOME():
     # ใช้ center_text เพื่อจัดข้อความให้อยู่ตรงกลาง
-    centered_intro = center_text(EMAIL)
+    centered_intro = center_text(HOME)
     for line in centered_intro.splitlines():
-        print(Fore.YELLOW + line)  # ทำให้ข้อความเป็นสีเหลือง
+        print(Fore.YELLOW + line + Style.RESET_ALL)  # ทำให้ข้อความเป็นสีเหลือง
         time.sleep(0.1)  # เพิ่มดีเลย์เพื่อจำลองแอนิเมชัน
 
 
