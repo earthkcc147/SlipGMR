563623bc3f4df80f50f3e17d9db34f5b23e081f4
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-23 13:24:57 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -990,7 +990,5 @@ def main_menu():
 if __name__ == "__main__":
     # clear_console()
     print_intro()
-    # รอการกด Enter เพื่อดำเนินการต่อ
-    input(Fore.GREEN + "\nกด Enter เพื่อดำเนินการต่อ..." + Style.RESET_ALL)
     start_menu()
     # main_menu()
